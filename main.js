01c38e0352f3e3021aa8980635ef4c4ef6c74bb7
Zekiru <zekiel.villasurda@gmail.com>
2023-11-08 11:00:03 +0800

    Complete Build

--- main.js
+++ main.js
@@ -7,16 +7,99 @@ const navigation = document.getElementById("navigation");
 const forwardbtn = document.getElementById("forward");
 const backbtn = document.getElementById("back");
 
-const clipMain = ["TestFolder/maxverstappen.mp4", "TestFolder/maxverstappen.mp4", "TestFolder/maxverstappen.mp4", "TestFolder/maxverstappen.mp4", "TestFolder/maxverstappen.mp4"];
+const decisionoverlay = document.getElementById("decision-overlay");
+const choice1 = document.getElementById("choice1");
+const choice2 = document.getElementById("choice2");
 
-const clipEnd = ["TestFolder/2023-11-06_21-39-13.mp4", "TestFolder/maxverstappen.mp4", "TestFolder/maxverstappen.mp4", "TestFolder/maxverstappen.mp4", "TestFolder/maxverstappen.mp4", "TestFolder/maxverstappen.mp4", "TestFolder/maxverstappen.mp4", "TestFolder/maxverstappen.mp4", "TestFolder/maxverstappen.mp4"];
+const clipMain = [
+    "TestFolder/maxverstappen.mp4",
+    "TestFolder/maxverstappen.mp4",
+    "TestFolder/maxverstappen.mp4",
+    "TestFolder/maxverstappen.mp4",
+    "TestFolder/maxverstappen.mp4"
+];
+
+const clipEnd = [
+    "TestFolder/2023-11-06_21-39-13.mp4",
+    "TestFolder/maxverstappen.mp4", "TestFolder/maxverstappen.mp4",
+    "TestFolder/maxverstappen.mp4", "TestFolder/maxverstappen.mp4",
+    "TestFolder/maxverstappen.mp4", "TestFolder/maxverstappen.mp4",
+    "TestFolder/maxverstappen.mp4", "TestFolder/maxverstappen.mp4"
+];
 
 const title = ["Prologue", "Ryan", "Ali", "Ana", "Siso", "Ending", "True Ending"];
 
+const decision = [
+    ["A", "B"],
+    ["C", "D"],
+    ["E", "F"],
+    ["G", "H"]
+];
+
 let playback = false;
 let navHover = false;
+let decoverHover = false;
+
+let decisionprompt = false;
+
 let progress = 0;
 let chosen = 0;
+
+function toggleDecision(status) {
+    if (progress <= 0) return;
+
+    playback = false;
+    if (status) {
+        pauseVid();
+        playbtn.setAttribute("hidden", "");
+        // forwardbtn.setAttribute("disabled", "");
+        choice1.removeAttribute("hidden");
+        choice2.removeAttribute("hidden");
+        decisionprompt = false;
+
+        choice1.innerHTML = decision[progress-1][0];
+        choice2.innerHTML = decision[progress-1][1];
+    } else {
+        forwardbtn.removeAttribute("disabled");
+        choice1.setAttribute("hidden", "");
+        choice2.setAttribute("hidden", "");
+    }
+}
+
+function chosenInit(prime) {
+    switch(progress) {
+        case 1:
+            chosen = 1;
+            break;
+        case 2:
+            chosen = 3;
+            break;
+        case 3:
+            chosen = 5;
+            break;
+        case 4:
+            chosen = 7;
+            break;
+        default:
+            break;
+    };
+
+    if (!prime) {
+        chosen += 1;
+    };
+}
+
+choice1.addEventListener("click", () => {
+    chosenInit(true);
+    toggleDecision(false);
+    nextVid();
+});
+
+choice2.addEventListener("click", () => {
+    chosenInit(false);
+    toggleDecision(false);
+    nextVid();
+});
 
 function playVid() {
     vid.play();
@@ -29,31 +112,81 @@ function pauseVid() {
 }
 
 function nextVid() {
-    if (progress >= 4) {
-        console.log("hello");
+    if (decisionprompt) {
+        toggleDecision(true);
+        return;
+    } else {
+        progress += 1;
+        toggleDecision(false);
+    };
+
+    if (progress >= 5) {
         if (chosen == 0) {
             console.log("good ending");
-            progress += 2;
+            progress += 1;
         } else {
             console.log("bad ending");
         };
         vidLoader(clipEnd[chosen]);
+        toggleDecision(false);
         navigation.setAttribute("hidden", "");
         return;
     };
-    progress += 1;
+    decisionprompt = true;
     vidLoader(clipMain[progress]);
+    backbtn.removeAttribute("disabled");
 }
 
 function previousVid() {
     if (progress <= 0) return;
-    progress -= 1;
+    if (decisionprompt) {
+        progress -= 1;
+        vidLoader(clipMain[progress]);
+        toggleDecision(true);
+        decisionprompt = false;
+        return
+    } else {
+        toggleDecision(false);
+        decisionprompt = true;
+    };
     vidLoader(clipMain[progress]);
+    if(progress <= 0) backbtn.setAttribute("disabled", "");
 }
 
 function progression() {
     document.getElementById("title").innerHTML = title[progress];
 }
+
+function vidLoader(video) {
+    source.setAttribute("src", video);
+    vid.load();
+    playVid();
+    progression();
+}
+
+
+vid.addEventListener("ended", () => {
+    if (progress >= 5) return;
+    nextVid();
+});
+
+
+overlay.addEventListener("click", () => {
+    if (!vid.paused) playback = true;
+    if (progress >= 5 && vid.ended || !playback || navHover || decoverHover || decisionprompt) return;
+
+    if (!source.hasAttribute("src")) {
+        vidLoader(clipMain[progress]);
+        return;
+    };
+
+    if (vid.paused) {
+        playVid();
+    } else {
+        pauseVid();
+    };
+    playback = false;
+});
 
 
 forwardbtn.addEventListener("mouseenter", () => {
@@ -80,46 +213,22 @@ playbtn.addEventListener("mouseleave", () => {
     playback = false;
 });
 
+decisionoverlay.addEventListener("mouseenter", () => {
+    decoverHover = true;
+});
+
+decisionoverlay.addEventListener("mouseleave", () => {
+    decoverHover = false;
+});
+
 
 backbtn.addEventListener("click", () => {
     previousVid();
-    if(progress <= 0) backbtn.setAttribute("disabled", "");
 });
 
 forwardbtn.addEventListener("click", () => {
     nextVid();
-    backbtn.removeAttribute("disabled");
 });
-
-overlay.addEventListener("click", () => {
-    if (!vid.paused) playback = true;
-    if (progress > 5 && vid.ended || !playback || navHover) return;
-
-    if (!source.hasAttribute("src")) {
-        vidLoader(clipMain[progress]);
-        return;
-    };
-
-    if (vid.paused) {
-        playVid();
-    } else {
-        pauseVid();
-    };
-    playback = false;
-});
-
-
-vid.addEventListener("ended", () => {
-    if (progress > 5) return;
-    nextVid();
-});
-
-function vidLoader(video) {
-    source.setAttribute("src", video);
-    vid.load();
-    playVid();
-    progression();
-}
 
 document.addEventListener('DOMContentLoaded', () => {
     progression();
